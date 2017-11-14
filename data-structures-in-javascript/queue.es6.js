e25d5e03e17b2d632e9190803dec297b65bb50b3
Abhishek kahol <abhishek4kahol@users.noreply.github.com>
2017-11-14 07:08:19 +0000

    Update queue.es6.js
    queue.peek comment correction

--- data-structures-in-javascript/queue.es6.js
+++ data-structures-in-javascript/queue.es6.js
@@ -30,7 +30,7 @@ queue.enqueue(2);
 queue.enqueue(3);
 queue.print(); // => 1 2 3
 console.log('length is 3:', queue.length()); // => 3
-console.log('peek is 1:', queue.peek()); // => 3
+console.log('peek is 1:', queue.peek()); // => 1
 console.log('dequeue is 1:', queue.dequeue()); // => 1
 queue.print(); // => 2 3
 console.log('dequeue is 2:', queue.dequeue());  // => 2
